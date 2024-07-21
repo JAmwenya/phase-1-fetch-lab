ccd51707555afe52f3acea1d5d563d105de937e7
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-07-21 13:21:13 +0300

    updates fetching

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
-  fetch("https://anapioficeandfire.com/api/books")
-  .then((resp) => resp.json())
-  .then((json) => renderBooks(json))  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json))
+    .catch((error) => console.error('Error fetching books:', error));
 }
 
 function renderBooks(books) {
